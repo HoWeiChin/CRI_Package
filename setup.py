1f0da0b8f73bd8798dfb179841db9e5d7a310649
HoWeiChin <hwc.howeichin@gmail.com>
2020-05-19 20:18:56 +0800

    latest version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-wc95", # Replace with your own username
-    version="0.0.1",
+    name="quicksort-wc95", # Replace with your own username
+    version="0.0.2",
     author="WeiChin",
     author_email="hwc.howeichin@gmail.com",
     description="A small example package",
